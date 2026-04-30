2e53e393aa0b09c91067ed685308eea724b5d3bf
Lemon <149338935+Lemon1151@users.noreply.github.com>
2026-04-30 23:46:46 +0800

    add Ubuntu 26.04 (Resolute) source

---
+++ config/suites/resolute.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+export RELASE_NAME="Ubuntu 26.04 (Resolute Raccoon)"
+export RELASE_VERSION="26.04"
+
+export KERNEL_REPO="https://github.com/Lemon1151/linux-rockchip-ubuntu.git"
+export KERNEL_BRANCH="resolute"
+export KERNEL_FLAVOR="rockchip"
+
+export EXTRA_PPAS="jjriek/rockchip"
