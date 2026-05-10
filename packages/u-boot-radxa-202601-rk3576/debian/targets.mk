bd6d1175b985ebb5f8f682a53e1e5e7e9f90e7ca
Lemon <149338935+Lemon1151@users.noreply.github.com>
2026-05-11 07:57:21 +0800

    debian/targets.mk: fix rock-4d and rock-4d-spi build failures

--- packages/u-boot-radxa-202601-rk3576/debian/targets.mk
+++ packages/u-boot-radxa-202601-rk3576/debian/targets.mk
@@ -2,13 +2,13 @@
 # debian/rules includes this Makefile snippet.
 
 u-boot-rockchip_platforms += rock-4d-rk3576
-armsom-sige5-rk3576_ddr := rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.03.bin
-armsom-sige5-rk3576_bl31 := rk3576_bl31_v1.04.elf
-armsom-sige5-rk3576_bl32 := rk3576_bl32_v1.01.bin
-armsom-sige5-rk3576_pkg := rock-4d
+rock-4d-rk3576_ddr := rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.03.bin
+rock-4d-rk3576_bl31 := rk3576_bl31_v1.04.elf
+rock-4d-rk3576_bl32 := rk3576_bl32_v1.01.bin
+rock-4d-rk3576_pkg := rock-4d
 
 u-boot-rockchip_platforms += rock-4d-spi-rk3576
-armsom-sige5-rk3576_ddr := rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.03.bin
-armsom-sige5-rk3576_bl31 := rk3576_bl31_v1.04.elf
-armsom-sige5-rk3576_bl32 := rk3576_bl32_v1.01.bin
-armsom-sige5-rk3576_pkg := rock-4d-spi
+rock-4d-spi-rk3576_ddr := rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.03.bin
+rock-4d-spi-rk3576_bl31 := rk3576_bl31_v1.04.elf
+rock-4d-spi-rk3576_bl32 := rk3576_bl32_v1.01.bin
+rock-4d-spi-rk3576_pkg := rock-4d-spi
