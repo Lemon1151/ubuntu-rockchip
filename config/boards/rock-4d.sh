6d7eb11da42f9b7a68743e30d668d9d9c9b1cb43
Lemon1151 <smile1151@foxmail.com>
2026-05-16 02:43:02 +0800

    Modify rock-4d.sh

--- config/boards/rock-4d.sh
+++ config/boards/rock-4d.sh
@@ -6,15 +6,14 @@ export BOARD_SOC="Rockchip RK3576"
 export BOARD_CPU="ARM Cortex A72 / A53"
 export UBOOT_PACKAGE="u-boot-radxa-202601-rk3576"
 export UBOOT_RULES_TARGET="rock-4d-rk3576"
-export UBOOT_RULES_TARGET_EXTRA="rock-4d-spi-rk3576"
 export COMPATIBLE_SUITES=("noble" "resolute")
 export COMPATIBLE_FLAVORS=("server" "desktop")
 
-function config_image_hook__rock-5d() {
+function config_image_hook__rock-4d() {
     local rootfs="$1"
 
     # Kernel modules to blacklist
-    echo "blacklist panfrost" > "${rootfs}/etc/modprobe.d/panfrost.conf"
+    # echo "blacklist panfrost" > "${rootfs}/etc/modprobe.d/panfrost.conf"
 
     return 0
 }
